540f2964da69f173122e66d56ed0b575d85e4332
burak öztürk <brkozt94@gmail.com>
2020-04-30 00:18:38 +0300

    cet322-homework3

---
+++ CetBookStore/ViewModel/SearchViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CetBookStore.ViewModel
+{
+    public class SearchViewModel
+    {
+         [Display(Name ="Arama Metni")]
+        public string SearchText { get; set; }
+        [Display(Name ="Açıklamalarda Ara")]
+        public bool SearchInDescription { get; set; }
+        [Display(Name ="Kategori Seçimi")]
+        public int? CategoryId { get; set; }
+
+        [Display(Name = "Minimum Fiyat Seçimi")]
+        public int? MinSearchPrice { get; set; }
+
+        [Display(Name = "Maximum Fiyat Seçimi")]
+        public int? MaxSearchPrice { get; set; }
+
+
+        public List<Models.Book> Results { get; set; }
+    }
+}
